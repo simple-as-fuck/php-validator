51d7c2260e879bba83bd4a145d7b8617d4d19c51
VojtaB <triplkrypl@centrum.cz>
2022-05-09 22:49:46 +0200

    ValueMust with required non-empty message

--- src/Model/ValueMust.php
+++ src/Model/ValueMust.php
@@ -6,7 +6,10 @@ namespace SimpleAsFuck\Validator\Model;
 
 final class ValueMust extends \Exception
 {
-    public function __construct(string $message = '')
+    /**
+     * @param non-empty-string $message
+     */
+    public function __construct(string $message)
     {
         parent::__construct($message);
     }
