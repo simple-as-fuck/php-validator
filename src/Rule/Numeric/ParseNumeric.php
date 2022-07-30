73fdee0af48c4bd0e5975fd3358341ed7d4c7eb6
VojtaB <triplkrypl@centrum.cz>
2022-07-30 15:38:16 +0200

    new methods for ParseNumeric validation

--- src/Rule/Numeric/ParseNumeric.php
+++ src/Rule/Numeric/ParseNumeric.php
@@ -11,6 +11,7 @@ use SimpleAsFuck\Validator\Model\ValueMust;
 use SimpleAsFuck\Validator\Rule\General\CastString;
 use SimpleAsFuck\Validator\Rule\General\Max;
 use SimpleAsFuck\Validator\Rule\General\ReadableRule;
+use SimpleAsFuck\Validator\Rule\General\Same;
 
 /**
  * @extends ReadableRule<string, numeric-string>
@@ -91,6 +92,49 @@ final class ParseNumeric extends ReadableRule
             'decimal digits'
         );
         return $maxRule;
+    }
+
+    /**
+     * @param positive-int $min digits before decimal separator, without minus sign
+     * @param positive-int $max digits before decimal separator, without minus sign
+     * @param int<0,max> $maxDecimal maximum digits after decimal separator
+     * @return Max<numeric-string, int>
+     */
+    public function betweenDigit(int $min, int $max, int $maxDecimal = 0): Max
+    {
+        return $this->minDigit($min)->maxDigit($max)->maxDecimal($maxDecimal);
+    }
+
+    /**
+     * @param positive-int $number digits before decimal separator, without minus sign
+     * @param int<0,max> $maxDecimal maximum digits after decimal separator
+     * @return Max<numeric-string, int>
+     */
+    public function exactDigit(int $number, int $maxDecimal = 0): Max
+    {
+        $same = new Same(
+            $this->exceptionFactory(),
+            /** @phpstan-ignore-next-line */
+            $this->ruleChain(),
+            $this->validated(),
+            $this->valueName(),
+            /** @phpstan-ignore-next-line */
+            new DigitCount(),
+            $number,
+            'digits before decimals'
+        );
+        /** @var Max<numeric-string, int> */
+        return new Max(
+            $same->exceptionFactory(),
+            $same->ruleChain(),
+            $same->validated(),
+            $same->valueName(),
+            /** @phpstan-ignore-next-line */
+            new DecimalCount(),
+            new CastString(),
+            $maxDecimal,
+            'decimal digits'
+        );
     }
 
     /**
