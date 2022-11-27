53610785fa1f495bc14c8c6eff05f77ed587edae
VojtaB <triplkrypl@centrum.cz>
2022-11-27 19:53:24 +0100

    regex rule deprecation removed and php 8.0 sintax improvement

--- src/Rule/String/Regex.php
+++ src/Rule/String/Regex.php
@@ -16,12 +16,6 @@ use SimpleAsFuck\Validator\Rule\General\ReadableRule;
  */
 final class Regex extends ReadableRule
 {
-    private string $pattern;
-    /** @var PREG_OFFSET_CAPTURE|PREG_UNMATCHED_AS_NULL|768|0 */
-    private int $flags;
-    /** @var array<string> */
-    private array $matches;
-
     /**
      * @param RuleChain<Tstring> $ruleChain
      * @param Validated<mixed> $validated
@@ -29,23 +23,15 @@ final class Regex extends ReadableRule
      * @param non-empty-string $pattern
      * @param PREG_OFFSET_CAPTURE|PREG_UNMATCHED_AS_NULL|768|0 $flags
      */
-    public function __construct(?Exception $exceptionFactory, RuleChain $ruleChain, Validated $validated, string $valueName, string $pattern, int $flags = 0)
-    {
+    public function __construct(
+        ?Exception $exceptionFactory,
+        RuleChain $ruleChain,
+        Validated $validated,
+        string $valueName,
+        private string $pattern,
+        private int $flags = 0
+    ) {
         parent::__construct($exceptionFactory, $ruleChain, $validated, $valueName);
-        $this->pattern = $pattern;
-        $this->flags = $flags;
-        $this->matches = [];
-    }
-
-    /**
-     * @deprecated will be removed use: ParseRegex::match (Validator::make()->string()->parseRegex()->match())
-     */
-    public function match(string $matchKey): RegexMatch
-    {
-        $this->validateChain();
-        /** @var Validated<mixed> $validated */
-        $validated = new Validated($this->matches);
-        return new RegexMatch($this->exceptionFactory(), new RuleChain(), $validated, $this->valueName().' regex: \''.$this->pattern.'\' match: \''.$matchKey.'\'', $matchKey);
     }
 
     /**
@@ -54,7 +40,7 @@ final class Regex extends ReadableRule
      */
     protected function validate($value): string
     {
-        $result = preg_match($this->pattern, $value, $this->matches, $this->flags);
+        $result = preg_match($this->pattern, $value, $matches, $this->flags);
         if ($result === 0) {
             throw new ValueMust('match regex: \''.$this->pattern.'\'');
         }
