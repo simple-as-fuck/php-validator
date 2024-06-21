60658ec1330ab9c0490a484a6d2b7be4cbe663c9
VojtaB <triplkrypl@centrum.cz>
2024-06-21 21:02:30 +0200

    make method for IntRule

--- src/Rule/Numeric/IntRule.php
+++ src/Rule/Numeric/IntRule.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace SimpleAsFuck\Validator\Rule\Numeric;
 
+use SimpleAsFuck\Validator\Factory\UnexpectedValueException;
+use SimpleAsFuck\Validator\Model\RuleChain;
+use SimpleAsFuck\Validator\Model\Validated;
 use SimpleAsFuck\Validator\Model\ValueMust;
 
 /**
@@ -11,6 +14,15 @@ use SimpleAsFuck\Validator\Model\ValueMust;
  */
 final class IntRule extends \SimpleAsFuck\Validator\Rule\General\IntRule
 {
+    /**
+     * @param non-empty-string $valueName
+     * @return \SimpleAsFuck\Validator\Rule\General\IntRule<mixed>
+     */
+    public static function make(mixed $value, string $valueName = 'variable'): \SimpleAsFuck\Validator\Rule\General\IntRule
+    {
+        return new IntRule(new UnexpectedValueException(), new RuleChain(), new Validated($value), $valueName);
+    }
+
     /**
      * @param mixed $value
      */
