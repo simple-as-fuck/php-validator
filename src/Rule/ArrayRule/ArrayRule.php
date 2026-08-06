5e2d679569a73b532079a7f1aa1aacf002319863
VojtaB <triplkrypl@centrum.cz>
2026-08-06 20:47:42 +0200

    ArrayRule make method added

--- src/Rule/ArrayRule/ArrayRule.php
+++ src/Rule/ArrayRule/ArrayRule.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 
 namespace SimpleAsFuck\Validator\Rule\ArrayRule;
 
+use SimpleAsFuck\Validator\Factory\UnexpectedValueException;
+use SimpleAsFuck\Validator\Model\RuleChain;
+use SimpleAsFuck\Validator\Model\Validated;
 use SimpleAsFuck\Validator\Model\ValueMust;
 
 final class ArrayRule extends \SimpleAsFuck\Validator\Rule\Common\ArrayRule
 {
+    /**
+     * @param non-empty-string $valueName
+     */
+    public static function make(mixed $value, string $valueName = 'variable'): \SimpleAsFuck\Validator\Rule\Common\ArrayRule
+    {
+        return new ArrayRule(
+            new UnexpectedValueException(),
+            new RuleChain(),
+            new Validated($value),
+            $valueName,
+        );
+    }
+
     /**
      * @param mixed $value
      * @return array<mixed>
